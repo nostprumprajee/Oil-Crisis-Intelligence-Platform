111294c2ca9e4212472e0f7b627e70f4d2e2d9b8
Wirawat Bunlom <wirawat@cardx.co.th>
2026-03-31 11:49:16 +0700

    fix : bug fixed

--- oil-crisis-frontend/app/api/oil/route.ts
+++ oil-crisis-frontend/app/api/oil/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  const res = await fetch("http://go-service:8080/oil-price");
+  const res = await fetch("http://localhost:8080/oil-price");
 //   const res = await fetch("http://localhost:8080/oil-price");
   const data = await res.json();
   return Response.json(data);
